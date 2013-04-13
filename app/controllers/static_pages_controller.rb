cb6de574a73423eefabf26412a2748eeb374f2fc
PHAM MINH NHAT <pmnhatdn@gmail.com>
2013-04-13 14:50:11 -0700

    Generate static_pages controller + passed test

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rorproject::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/blog"
+
+  get "static_pages/help"
+  root :to => 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
