18bd64f5259054ab07bd9248ded2dc7530957445
PHAM MINH NHAT <pmnhatdn@gmail.com>
2013-04-13 22:41:21 -0700

    Edit static pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,18 @@ class SessionsController < ApplicationController
 	end
 	
 	#Signin : create a new session
-	def create	
-
+	def create				
 		user =User.authenticate(params[:session][:username],params[:session][:password])		
 		if user 			
 			sign_in user			
-			redirect_to user, :notice => "Login successfully"
+			#redirect_to user, :notice => "Login successfully"
+			redirect_back_or user
 			
 	  	else
-            flash[:error] = 'Invalid username or password'
-	  		render 'new'
+	        flash[:error] = 'Invalid username or password'
+	 		render 'new'
 	    end  		  
+	
 	end
 	
 	#Signout: call sign_out from sessionhelper
